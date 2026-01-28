c934558fd00f68bb4aa90bc1d53e7205c89f168c
Pranav <pranavdesai698@gmail.com>
2026-01-28 23:03:07 +0530

    Backend ready for deployment

--- bunkchat-server/server.js
+++ bunkchat-server/server.js
@@ -1,6 +1,7 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 3000 });
+const PORT = process.env.PORT || 3000;
+const wss = new WebSocket.Server({ port: PORT });
 
 // -------------------------------------------------
 // GLOBAL IN-MEMORY STATE
